e09f21cccaafe7fd5cec051419ed1fa46f1e874e
narendar <nkanchala@rich.com>
2025-12-26 17:31:52 +0530

    Add application code to develop branch

---
+++ core/diagnostics/DiagnosticScenarios/Program.cs
@@ -0,0 +1,10 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
